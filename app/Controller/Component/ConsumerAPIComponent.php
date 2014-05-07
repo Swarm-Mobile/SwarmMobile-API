b5d2136ea757f01c4a36df1e5e42c803d3317213
Jinesh Lalan <jineshlalan@ip-10-160-59-252.us-west-1.compute.internal>
2014-05-07 22:42:27 +0000

    Initial Path Array

--- app/Controller/Component/ConsumerAPIComponent.php
+++ app/Controller/Component/ConsumerAPIComponent.php
@@ -4,7 +4,34 @@ require_once 'DBComponent.php';
 
 class ConsumerAPIComponent extends APIComponent{
     
-    public $paths = array();
+    public $paths = array(
+		'Person' => array(
+			'Purchases' => array(),
+			'Products'  => array('Purchases'),
+			'Locations' => array('Purchases', 'Products'),
+			'Brands'    => array('Purchases', 'Products')
+		),
+		'Purchases' => array(
+			'Products'  => array(),
+			'Locations' => array('Products'),
+			'Brands'    => array('Products'),
+		),
+		'Products'  => array (
+			'Locations'	=> array(),
+			'Brands'    => array(),
+		),
+		'Brands'  => array(
+			'Person'    => array('Products', 'Purchases'),
+			'Products'  => array(''),
+			'Locations' => array('')
+		),
+		'Visits'  => array(
+			'Person'    => array()
+		),
+		'Devices' => array(),
+		
+	
+	);
     
     public function __call($name, $arguments) {
         
