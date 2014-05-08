da32145b53d89afb411c9aa11a48eec6a4ded3aa
Jinesh Lalan <jineshlalan@ip-10-160-59-252.us-west-1.compute.internal>
2014-05-08 00:03:43 +0000

    updated path array

--- app/Controller/Component/ConsumerAPIComponent.php
+++ app/Controller/Component/ConsumerAPIComponent.php
@@ -3,39 +3,49 @@
 require_once 'DBComponent.php';
 
 class ConsumerAPIComponent extends APIComponent{
-    
+
     public $paths = array(
-		'Person' => array(
-			'Purchase' => array(),
-			'Product'  => array('Purchase'),
-			'Location' => array('Purchase', 'Product'),
-			'Brand'    => array('Purchase', 'Product')
-		),
-		'Purchase' => array(
-			'Product'  => array(),
-			'Location' => array('Product'),
-			'Brand'    => array('Product'),
-		),
-		'Product'  => array (
-			'Location'	=> array(),
-			'Brand'    => array(),
-		),
-		'Brand'  => array(
-			'Person'    => array('Product', 'Purchase'),
-			'Product'  => array(''),
-			'Location' => array('')
-		),
-		'Visit'  => array(
-			'Person'    => array(),
-			'Device'   => array()
-		),
-		'Device' => array(
-			'Person'  => array()
-		),
-	);
+        'Person' => array(
+            'Purchase' => array(),
+            'Product'  => array('Purchase'),
+            'Location' => array('Purchase', 'Product'),
+            'Brand'    => array('Purchase', 'Product'),
+        ),
+        'Purchase' => array(
+            'Product'  => array(),
+            'Location' => array('Product'),
+            'Brand'    => array('Product'),
+            'Person'   => array()
+        ),
+        'Product'  => array (
+            'Location' => array(),
+            'Brand'    => array(),
+            'Person'   => array('Purchase'),
+            'Purchase' => array()
+        ),
+        'Brand'  => array(
+            'Person'    => array('Product', 'Purchase'),
+            'Product'  => array(),
+            'Location' => array()
+        ),
+        'Visit'  => array(
+            'Person'   => array(),
+            'Device'   => array(),
+            'Location' => array()
+        ),
+        'Device' => array(
+            'Person'  => array()
+        ),
+        'Location'  => array(
+            'Person'   => array('Product', 'Purchase'),
+            'Product'  => array(),
+            'Purchase' => array('Product'),
+            'Brand'    => array(),
+            'Visit'    => array()
+        )
+    );
     
     public function __call($name, $arguments) {
         
     }
-
 }
