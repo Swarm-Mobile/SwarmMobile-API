bbb6e5dadccad70e6c59297e31c0b8247f599e42
ivgam <ivan.garcia.maya@gmail.com>
2014-05-12 17:39:40 -0700

    paralelize request

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -7,27 +7,10 @@ Router::connect('/request_client'   , array('controller' => 'api'   , 'action' =
 Router::connect('/pages/*'          , array('controller' => 'pages' , 'action' => 'display'));
 Router::connect('/users/:action'    , array('controller' => 'users'));
 Router::connect('/inbox/:action'    , array('controller' => 'inbox'));
-$subdomain = strstr($_SERVER['HTTP_HOST'], '.', true);
-switch ($subdomain) {
-    case 'api':
-    case 'intapi':
-    case 'devapi':
-    case 'newapi':
-        Router::connect('/logout', array('controller' => 'api', 'action' => 'logout'));
-        Router::connect('/login', array('controller' => 'api', 'action' => 'login'));
-        Router::connect('/admin', array('controller' => 'admin', 'action' => 'home'));
-        Router::connect('/*', array('controller' => 'api', 'action' => 'index'));
-    case 'jineshapi':
-        Router::connect('/logout', array('controller' => 'api', 'action' => 'logout'));
-        Router::connect('/login', array('controller' => 'api', 'action' => 'login'));
-        Router::connect('/admin', array('controller' => 'api', 'action' => 'home'));
-        Router::connect('/users', array('controller' => 'users', 'action' => 'index'));
-        Router::connect('/api/*', array('controller' => 'api', 'action' => 'index'));
-        break;
-    default:
-        Router::connect('/api/*', array('controller' => 'api', 'action' => 'index'));
-        break;
-}
+Router::connect('/logout', array('controller' => 'api', 'action' => 'logout'));
+Router::connect('/login', array('controller' => 'api', 'action' => 'login'));
+Router::connect('/admin', array('controller' => 'admin', 'action' => 'home'));
+Router::connect('/*', array('controller' => 'api', 'action' => 'index'));
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
