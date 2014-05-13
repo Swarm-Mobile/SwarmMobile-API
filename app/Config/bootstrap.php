c30d496d9d778952c5e9eb4fb4dd13b5fa59d3a3
ivgam <ivan.garcia.maya@gmail.com>
2014-05-13 14:49:29 -0700

    set apc

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -23,7 +23,12 @@
  */
 
 // Setup a 'default' cache configuration for use in the application.
-Cache::config('default', array('engine' => 'File'));
+Cache::config('default', array(
+    'engine' => 'Apc', //[required]
+    'duration' => 3600, //[optional]
+    'probability' => 100, //[optional]
+    'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+));
 
 /**
  * The settings below can be used to set additional paths to models, views and controllers.
