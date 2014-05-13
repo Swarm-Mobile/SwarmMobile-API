b033fb70b4eba06cb8abaaa0528b91cd94453872
ivgam <ivan.garcia.maya@gmail.com>
2014-05-13 14:51:57 -0700

    permissions

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -24,10 +24,8 @@
 
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array(
-    'engine' => 'Apc', //[required]
-    'duration' => 3600, //[optional]
-    'probability' => 100, //[optional]
-    'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+    'engine' => 'File',
+    'mask' => 0777,
 ));
 
 /**
