d095965472d9e2413aa0d2d56bc1f60d656c2f1e
ivgam <ivan.garcia.maya@gmail.com>
2014-06-18 14:17:58 -0700

    rename classname

--- app/Controller/Component/DeviceComponent.php
+++ app/Controller/Component/DeviceComponent.php
@@ -2,8 +2,9 @@
 
 App::uses('APIComponent', 'Controller/Component');
 
-class NetworkComponent extends APIComponent {
-    public function visitorEvent(){        
+class DeviceComponent extends APIComponent {
+    
+    public function visitorEvent($params){        
         if(!empty($_POST)){
             $file = __DIR__.'/../../../raw/'.date('Y_m_d_h_i_s_').uniqid();
             $content = var_export($_POST);
