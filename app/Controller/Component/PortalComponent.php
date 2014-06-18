3e62130147e6fcec30d64641e06966231545f1b9
ivgam <ivan.garcia.maya@gmail.com>
2014-06-18 15:04:04 -0700

    device to Portal

--- app/Controller/Component/PortalComponent.php
+++ app/Controller/Component/PortalComponent.php
@@ -3,13 +3,13 @@
 App::uses('APIComponent', 'Controller/Component');
 
 class PortalComponent extends APIComponent {
-    
-    public function visitorEvent($params){        
+
+    public function visitorEvent($params){
         if(!empty($_POST)){
-            $file = __DIR__.'/../../../raw/'.date('Y_m_d_h_i_s_').uniqid();
-            $content = var_export($_POST);
+            $file = realpath(__DIR__.'/../../../../').'/raw/'.date('Y_m_d_h_i_s_').uniqid();
+            $content = var_export($_POST,true);
             file_put_contents($file, $content);
         }
-        return array();
+        return array('path'=>$file);
     }
 }
