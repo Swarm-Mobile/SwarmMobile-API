d8e648d2f0b892c52e6462bb64fca4591be80599
ivgam <ivan.garcia.maya@gmail.com>
2014-06-18 14:22:50 -0700

    add new component

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -9,6 +9,7 @@ App::uses('Model', 'Model');
 App::uses('RequestModel', 'Model');
 
 //ONE for every component that extends from APIComponent
+App::uses('DeviceComponent', 'Controller/Component');
 App::uses('ConsumerComponent', 'Controller/Component');
 App::uses('MemberComponent', 'Controller/Component');
 App::uses('NetworkComponent', 'Controller/Component');
