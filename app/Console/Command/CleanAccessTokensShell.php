625fd29a518a8cea6338e7359c051af367ba4e74
ivgam <ivan.garcia.maya@gmail.com>
2014-07-14 15:13:40 -0700

    clean access tokens script

--- app/Console/Command/CleanAccessTokensShell.php
+++ app/Console/Command/CleanAccessTokensShell.php
@@ -1,11 +1,14 @@
 <?php
 
+require_once(__DIR__ . '/../../Controller/Component/DBComponent.php');
 App::uses('AppShell', 'Console/Command');
+App::uses('Model', 'Model');
 
 class CleanAccessTokensShell extends AppShell {
 
     public function main() {
-        $sSQL = "DELETE FROM access_tokens WHERE NOW() > FROM_UNIXTIME(expires)";
+        $week = 3600 * 24 * 7;
+        $sSQL = "DELETE FROM access_tokens WHERE NOW() > FROM_UNIXTIME(expires) + $week";
         $oModel = new Model(false, 'refresh_tokens', 'oauth');
         $oDb = $oModel->getDataSource();
         $oDb->query($sSQL);
