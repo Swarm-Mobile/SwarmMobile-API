c72d63c9f2f6b9d6fd7a3d16f85bb2d55be2cdc1
ivgam <ivan.garcia.maya@gmail.com>
2014-07-14 15:18:41 -0700

    clean access tokens set environment

--- app/Console/Command/CleanAccessTokensShell.php
+++ app/Console/Command/CleanAccessTokensShell.php
@@ -6,7 +6,21 @@ App::uses('Model', 'Model');
 
 class CleanAccessTokensShell extends AppShell {
 
+    private function setEnvironment($env = false) {
+        if (!$env) {
+            $htaccess = file_get_contents(__DIR__ . '/../../../.htaccess');
+            $pattern = '/.*SetEnv server_location "(.*)"/';
+            if (preg_match_all($pattern, $htaccess, $matches)) {
+                putenv('server_location=' . $matches[1][0]);
+                $_SERVER['server_location'] = $matches[1][0];
+            }
+        } else {
+            $_SERVER['server_location'] = $env;
+        }
+    }
+
     public function main() {
+        setEnvironment();
         $week = 3600 * 24 * 7;
         $sSQL = "DELETE FROM access_tokens WHERE NOW() > FROM_UNIXTIME(expires) + $week";
         $oModel = new Model(false, 'refresh_tokens', 'oauth');
@@ -14,5 +28,5 @@ class CleanAccessTokensShell extends AppShell {
         $oDb->query($sSQL);
         $this->out("Done!");
     }
-    
+
 }
