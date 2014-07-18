afa084e3a0e3fc19e02d7345f722b1413c5e67d9
Ubuntu <ubuntu@ip-10-161-113-4.us-west-1.compute.internal>
2014-07-18 21:29:38 +0000

    database configuration

--- app/Config/database.php
+++ app/Config/database.php
@@ -264,7 +264,7 @@ class DATABASE_CONFIG {
         'backstage' => array(
             'datasource' => 'Database/Mysql',
             'persistent' => true,
-            'host' => 'swarm-hex.cdmer9ay9s4r.us-west-1.rds.amazonaws.com:3306',
+            'host' => 'swarm-hex.cdmer9ay9s4r.us-west-1.rds.amazonaws.com',
             'login' => 'swarm_ios',
             'password' => 'F6gatRad',
             'database' => 'swarm_backstage',
@@ -282,7 +282,7 @@ class DATABASE_CONFIG {
         'rollups' => array(
             'datasource' => 'Database/Mysql',
             'persistent' => true,
-            'host' => 'swarm-hex.cdmer9ay9s4r.us-west-1.rds.amazonaws.com:3306',
+            'host' => 'swarm-hex.cdmer9ay9s4r.us-west-1.rds.amazonaws.com',
             'login' => 'swarm_ios',
             'password' => 'F6gatRad',
             'database' => 'rollups',
