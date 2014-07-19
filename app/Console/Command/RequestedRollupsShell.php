3ca6f8d9c2bf585f3ae2ef44484e2ce8c4e65d6e
ivgam <ivan.garcia.maya@gmail.com>
2014-07-19 11:39:15 -0700

    requested rollups shell

---
+++ app/Console/Command/RequestedRollupsShell.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once(__DIR__ . '/../../Controller/ApiController.php');
+require_once(__DIR__ . '/../../Controller/Component/DBComponent.php');
+require_once(__DIR__ . '/RollupShell.php');
+App::uses('APIComponent', 'Controller/Component');
+App::uses('AppShell', 'Console/Command');
+App::uses('Model', 'Model');
+App::uses('CakeEmail', 'Network/Email');
+
+class RequestedRollupsShell extends AppShell {
+
+    public function mark_as_processed($oRow) {
+        $oDb = DBComponent::getInstance('request_rollups_queue', 'rollups');
+        $sSQL = <<<SQL
+INSERT INTO request_rollups_processed
+    SET location_id     = :location_id,
+        start_date      = :start_date,        
+        end_date        = :end_date,        
+        override        = :override,        
+        rebuild         = :rebuild,        
+        reporter_email  = :reporter_email,        
+        ts_queue        = :ts_queue,        
+        ts              = NOW()
+SQL;
+        $oDb->query($sSQL, [
+            ':location_id' => $oRow['q']['location_id'],
+            ':start_date' => $oRow['q']['start_date'],
+            ':end_date' => $oRow['q']['end_date'],
+            ':override' => $oRow['q']['override'],
+            ':rebuild' => $oRow['q']['rebuild'],
+            ':reporter_email' => $oRow['q']['reporter_email'],
+            ':ts_queue' => $oRow['q']['ts'],
+        ]);
+        $oDb->query('DELETE FROM request_rollups_queue WHERE id = :id', [':id' => $oRow['q']['id']]);
+    }
+
+    public function send_email($oRow) {
+        $timestamp = date('Y-m-d H:i:s');
+        $Email = new CakeEmail();
+        $Email->from(array('info@swarm-mobile.com' => 'Info'));
+        $Email->to($oRow['q']['reporter_email']);
+        $Email->subject('Requested Rollup for Location #' . $oRow['q']['location_id'] . ' finished');
+        $text = <<<TEXT
+Rollup Details:
+Location # {$oRow['q']['location_id']}
+Start Date {$oRow['q']['start_date']}
+End Date {$oRow['q']['end_date']}
+Override {$oRow['q']['override']}
+Rebuild {$oRow['q']['rebuild']}
+
+Time Requested: {$oRow['q']['ts']}
+Time Processed: {$timestamp}
+TEXT;
+        $Email->send();        
+    }
+
+    public function main() {        
+        $this->setEnvironment();
+        $oDb = DBComponent::getInstance('request_rollups_queue', 'rollups');
+        $aRes = $oDb->fetchAll("SELECT * FROM request_rollups_queue q");
+        if (!empty($aRes)) {
+            foreach ($aRes as $oRow) {
+                $oRollup = new RollupShell();
+                $oRollup->params['location_id'] = $oRow['q']['location_id'];
+                $oRollup->params['start_date'] = $oRow['q']['start_date'];
+                $oRollup->params['end_date'] = $oRow['q']['end_date'];
+                $oRollup->params['override'] = $oRow['q']['override'];
+                $oRollup->params['rebuild'] = $oRow['q']['rebuild'];
+                $oRollup->main();
+                $this->mark_as_processed($oRow);
+                $this->send_email($oRow);
+            }
+        }
+    }
+
+}
