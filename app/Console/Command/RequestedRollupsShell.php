c2677d3bb19bd865e33a529ed14c9cfd652e4476
ivgam <ivan.garcia.maya@gmail.com>
2014-07-19 11:41:19 -0700

    add set environment function

--- app/Console/Command/RequestedRollupsShell.php
+++ app/Console/Command/RequestedRollupsShell.php
@@ -10,6 +10,19 @@ App::uses('CakeEmail', 'Network/Email');
 
 class RequestedRollupsShell extends AppShell {
 
+    private function setEnvironment($env = false) {
+        if (!$env) {
+            $htaccess = file_get_contents(__DIR__ . '/../../../.htaccess');
+            $pattern = '/.*SetEnv server_location "(.*)"/';
+            if (preg_match_all($pattern, $htaccess, $matches)) {
+                putenv('server_location=' . $matches[1][0]);
+                $_SERVER['server_location'] = $matches[1][0];
+            }
+        } else {
+            $_SERVER['server_location'] = $env;
+        }
+    }
+
     public function mark_as_processed($oRow) {
         $oDb = DBComponent::getInstance('request_rollups_queue', 'rollups');
         $sSQL = <<<SQL
@@ -52,10 +65,10 @@ Rebuild {$oRow['q']['rebuild']}
 Time Requested: {$oRow['q']['ts']}
 Time Processed: {$timestamp}
 TEXT;
-        $Email->send();        
+        $Email->send();
     }
 
-    public function main() {        
+    public function main() {
         $this->setEnvironment();
         $oDb = DBComponent::getInstance('request_rollups_queue', 'rollups');
         $aRes = $oDb->fetchAll("SELECT * FROM request_rollups_queue q");
