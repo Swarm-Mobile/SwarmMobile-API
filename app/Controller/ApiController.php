850c690979c3c0d464528ca286a2ee4810d18fb4
ivgam <ivan.garcia.maya@gmail.com>
2014-07-22 19:43:55 -0700

    Add json content type

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -114,7 +114,7 @@ class APIController extends AppController {
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: POST, GET");
         header("Access-Control-Allow-Headers: X-PINGOTHER");
-        header("Access-Control-Max-Age: 1728000");
+        header("Content-Type: application/json; charset=UTF-8");        
         header("Access-Control-Max-Age: 1728000");
         header("Pragma: no-cache");
         header("Cache-Contro;: no-store; no-cache;must-revalidate; post-check=0; pre-check=0");
