f9ebcc3d19330a675fc557e41bccb031d9f10565
ivgam <ivan.garcia.maya@gmail.com>
2014-07-25 09:39:32 -0700

    json headers oauth endpoints

--- plugins/OAuth/Controller/OAuthController.php
+++ plugins/OAuth/Controller/OAuthController.php
@@ -26,20 +26,13 @@ class OAuthController extends OAuthAppController {
      * beforeFilter
      *
      */
-    public function beforeFilter() {
+    public function beforeFilter() {        
         parent::beforeFilter();
         $this->OAuth->authenticate = array('fields' => array('username' => 'email'));
         $this->Auth->allow($this->OAuth->allowedActions);
         if (!isset($this->Security)) {
             $this->Security->blackHoleCallback = 'blackHole';
         }
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: POST, GET");
-        header("Access-Control-Allow-Headers: X-PINGOTHER");
-        header("Content-Type: application/json; charset=UTF-8");        
-        header("Access-Control-Max-Age: 1728000");
-        header("Pragma: no-cache");
-        header("Cache-Control: no-store; no-cache;must-revalidate; post-check=0; pre-check=0");
     }
 
     /**
--- vendors/oauth2-php/lib/OAuth2.php
+++ vendors/oauth2-php/lib/OAuth2.php
@@ -1115,12 +1115,16 @@ class OAuth2 {
 	 * @ingroup oauth2_section_5
 	 */
 	private function sendJsonHeaders() {
-		if (php_sapi_name() === 'cli' || headers_sent()) {
-			return;
-		}
-		
-		header("Content-Type: application/json");
-		header("Cache-Control: no-store");
+//		if (php_sapi_name() === 'cli' || headers_sent()) {
+//			return;
+//		}
+            header("Access-Control-Allow-Origin: *");
+            header("Access-Control-Allow-Methods: POST, GET");
+            header("Access-Control-Allow-Headers: X-PINGOTHER");
+            header("Content-Type: application/json; charset=UTF-8");        
+            header("Access-Control-Max-Age: 1728000");
+            header("Pragma: no-cache");
+            header("Cache-Control: no-store; no-cache;must-revalidate; post-check=0; pre-check=0");
 	}
 
 	/**
