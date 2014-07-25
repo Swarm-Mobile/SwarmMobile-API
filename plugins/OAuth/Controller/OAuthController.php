d31a5e459c370857ede9ae338df048719ab69d52
ivgam <ivan.garcia.maya@gmail.com>
2014-07-25 09:23:47 -0700

    json headers oauth endpoints

--- plugins/OAuth/Controller/OAuthController.php
+++ plugins/OAuth/Controller/OAuthController.php
@@ -27,6 +27,12 @@ class OAuthController extends OAuthAppController {
      *
      */
     public function beforeFilter() {
+        parent::beforeFilter();
+        $this->OAuth->authenticate = array('fields' => array('username' => 'email'));
+        $this->Auth->allow($this->OAuth->allowedActions);
+        if (!isset($this->Security)) {
+            $this->Security->blackHoleCallback = 'blackHole';
+        }
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: POST, GET");
         header("Access-Control-Allow-Headers: X-PINGOTHER");
@@ -34,12 +40,6 @@ class OAuthController extends OAuthAppController {
         header("Access-Control-Max-Age: 1728000");
         header("Pragma: no-cache");
         header("Cache-Control: no-store; no-cache;must-revalidate; post-check=0; pre-check=0");
-        parent::beforeFilter();
-        $this->OAuth->authenticate = array('fields' => array('username' => 'email'));
-        $this->Auth->allow($this->OAuth->allowedActions);
-        if (!isset($this->Security)) {
-            $this->Security->blackHoleCallback = 'blackHole';
-        }
     }
 
     /**
