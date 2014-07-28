8806aa75762e4809d3f7297f894e13e65bed0696
ivgam <ivan.garcia.maya@gmail.com>
2014-07-28 10:22:07 -0700

    fill conversionRate

--- app/Controller/Component/LocationComponent.php
+++ app/Controller/Component/LocationComponent.php
@@ -390,7 +390,7 @@ SQL;
         
         $result['data']['totals']['revenue'] += $aRes[0][0]['revenue'];
         $result['data']['totals']['transactions'] += $aRes[0][0]['transactions'];
-        $result['data']['totals']['conversionRate'] += $aRes[0][0]['conversionRate'];
+        $result['data']['totals']['conversionRate'] += $aRes[0][0]['transactions'];
 
         $oDb = DBComponent::getInstance('visitorEvent', 'portal');
         $sSQL = <<<SQL
