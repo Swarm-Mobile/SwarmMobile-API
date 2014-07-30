63a974b10ed4acb6e25bed30981bcce1f28c7e02
ivgam <ivan.garcia.maya@gmail.com>
2014-07-30 10:13:20 -0700

    Email Queue Component Shell process

--- app/Controller/Component/EmailQueueComponent.php
+++ app/Controller/Component/EmailQueueComponent.php
@@ -54,18 +54,21 @@ SQL;
                     ':ts_queue' => $oRow['e']['ts'],
                         ]
                 );
-                $oDb->query("DELETE FROM email_queue WHERE id = :id", [':id' => $oRow['e']['id']]);
-
-                $oEmail = new CakeEmail();
-                $oEmail->from($oRow['e']['from_email'],$oRow['e']['from_name']);
+                $oDb->query("DELETE FROM email_queue WHERE id = :id", [':id' => $oRow['e']['id']]);                
+                $oEmail = new CakeEmail('smtp');
+                $oEmail->emailFormat("html");
                 $oEmail->subject($oRow['e']['subject']);
-                $oEmail->to($oRow['e']['to_email'],$oRow['e']['to_name']);
-                $oEmail->send($oRow['e']['message']);
-            } catch (Exception $e) {
-                $oEmail = new CakeEmail();
-                $oEmail->from('info@swarm-mobile.com','Info');
-                $oEmail->subject($oRow['e']['subject']);
-                $oEmail->to('dev@swarm-mobile.com','Dev');
+                $oEmail->from([$oRow['e']['from_email']=>$oRow['e']['from_name']]);                
+                $oEmail->to([$oRow['e']['to_email']=>$oRow['e']['to_name']]);                
+                $oEmail->send($oRow['e']['message']);           
+                echo 'ok';     
+            } catch (Exception $e) {                
+                echo 'ko';
+                $oEmail = new CakeEmail('smtp');
+                $oEmail->emailFormat("html");
+                $oEmail->from(['info@swarm-mobile.com'=>'Info']);
+                $oEmail->subject('Error Occur sending email');
+                $oEmail->to(['dev@swarm-mobile.com'=>'Dev']);
                 $message = <<<EMAIL
 An error ocurred when the email queue try to send the next email
 FROM    : <{$oRow['e']['from_email']}>{$oRow['e']['from_name']}
