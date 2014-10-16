73ddde5f787d49a9f6d1c46442e7bc12ad114f67
Jinesh Lalan <jineshlalan@ip-10-200-153-198.us-west-1.compute.internal>
2014-10-16 23:17:54 +0000

    Ping As Presence Queue

---
+++ app/Config/redis.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'oAuth' => [
+      'host' => 'tcp://authserver.mddcrl.0001.usw1.cache.amazonaws.com',
+  ],    
+    'pingAsPresence' => [
+        //'host' => 'tcp://ping-as-presence.mddcrl.0001.usw1.cache.amazonaws.com',
+        'host' => 'tcp://localhost',
+    ],
+];
