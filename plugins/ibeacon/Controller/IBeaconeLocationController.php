8c32ce5f991987942e67015f820f37bbe7036a1f
Jinesh Lalan <jineshlalan@ip-10-200-153-198.us-west-1.compute.internal>
2014-10-28 19:48:35 +0000

    Moved pingAsPresence to what_is_here endpoint

--- plugins/ibeacon/Controller/IBeaconeLocationController.php
+++ plugins/ibeacon/Controller/IBeaconeLocationController.php
@@ -12,9 +12,7 @@
  */
 
 App::uses('IBeaconController', 'ibeacon.Controller');
-App::uses('RedisComponent', 'ibeacon.Controller/Component');
 App::uses('IBeaconDeviceCoordinate', 'ibeacon.Model');
-App::uses('User', 'ibeacon.Model/PingAsPresence');
 
 class IBeaconeLocationController extends  IBeaconController {
 
@@ -51,89 +49,9 @@ class IBeaconeLocationController extends  IBeaconController {
                     );
                     $location['categorization'] = $categorys;
                     $response['locations'][] = $location;
-                    $this->pingAsPresence($location['id'], $deviceId);
                 }
             }
         }
         echo json_encode($response);exit;
-    }
-
-    /**
-     * Ping as Presence
-     * 
-     * Inserts data into redis
-     * @param Integer locationId
-     * @param Integer deviceId
-     */
-    private function pingAsPresence($locationId, $deviceId)
-    {
-        $email = $this->request->data['user_id'];
-        $lat   = (!empty($this->request->data['locations'][0]['latitude'])) ? $this->request->data['locations'][0]['latitude'] : '';
-        $long  = (!empty($this->request->data['locations'][0]['longitude'])) ? $this->request->data['locations'][0]['longitude'] : '';
-        $rssi  = (!empty($this->request->data['locations'][0]['rssi'])) ? $this->request->data['locations'][0]['rssi'] : '';
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-        if(empty($email)) return;
-        $userModel = new User();
-        $user = $userModel->findByEmail($email);
-        if(empty($user)) {
-            $userModel->set('email', $email);
-            $user = $userModel->save();
-        }
-        try {
-            $redis = RedisComponent::getInstance('pingAsPresence');
-        } catch (Exception $e) {
-            return;
-        }
-        if(empty($locationId)) return;
-        $hKey = $locationId . ':' . $user['User']['id'];
-        $hDeviceKey  = 'device:' . $deviceId;
-        $data = $redis->hgetall($hKey);
-        $date = new DateTime('now', new DateTimeZone('GMT'));
-        $currentTime = $date->format('Y-m-d H:i:s');
-        $currentTimestamp = $date->getTimestamp();
-        $dataSet = array();
-        if (!empty($data['timestamp'])) {
-            $timeDiff = round(abs(strtotime($currentTime) - strtotime($data['timestamp'])) / 60, 2);
-            if ($timeDiff < 20) {
-                $dataSet[$hDeviceKey] = (!empty($data[$hDeviceKey])) ? $data[$hDeviceKey] . '$' . $currentTimestamp : $currentTimestamp;
-                $dataSet['timestamp'] = $currentTime;
-                $dataSet[$hDeviceKey] .= ':' . $lat . ':' . $long . ':' . $rssi;
-                $redis->hmset($hKey, $dataSet);
-                return;
-            } else {
-                if (empty($data['count'])) {
-                    $dataSet['count'] = 1;
-                    $dataSet['timestamp-1'] = $currentTime;
-                    $hDeviceKey = $hDeviceKey . '-1';
-                    $dataSet[$hDeviceKey] = $currentTimestamp;
-                    $dataSet[$hDeviceKey] .= ':' . $lat . ':' . $long . ':' . $rssi;
-                    $redis->hmset($hKey, $dataSet);
-                    return;
-                } else {
-                    $timeDiff = round(abs(strtotime($currentTime) - strtotime($data['timestamp-' . $data['count']])) / 60, 2);
-                    if ($timeDiff < 20) {
-                        $hDeviceKey = $hDeviceKey . '-' . $data['count'];
-                        $dataSet[$hDeviceKey] = (!empty($data[$hDeviceKey])) ? $data[$hDeviceKey] . '$' . $currentTimestamp : $currentTimestamp;
-                        $dataSet['timestamp-' . $data['count']] = $currentTime;
-                        $dataSet[$hDeviceKey] .= ':' . $lat . ':' . $long . ':' . $rssi;
-                        $redis->hmset($hKey, $dataSet);
-                        return;
-                    } else {
-                        $dataSet['count'] = $data['count'] + 1;
-                        $hDeviceKey = $hDeviceKey . '-' . $dataSet['count'];
-                        $dataSet[$hDeviceKey] = $currentTimestamp;
-                        $dataSet['timestamp-' . $dataSet['count']] = $currentTime;
-                        $dataSet[$hDeviceKey] .= ':' . $lat . ':' . $long . ':' . $rssi;
-                        $redis->hmset($hKey, $dataSet);
-                        return;
-                    }
-                }
-            }
-        }
-        $dataSet['timestamp'] = $currentTime;
-        $dataSet[$hDeviceKey] = $currentTimestamp;
-        $dataSet[$hDeviceKey] .= ':' . $lat . ':' . $long . ':' . $rssi;
-        $redis->hmset($hKey, $dataSet);
-        return;
     }
 }
