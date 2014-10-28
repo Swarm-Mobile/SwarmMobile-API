175ccd95f04ae9c5dce5547e453e6bc0987f77ac
Ubuntu <ubuntu@ip-10-161-45-153.us-west-1.compute.internal>
2014-10-28 01:02:32 +0000

    Redis.config syntax error

--- app/Config/redis.php
+++ app/Config/redis.php
@@ -2,17 +2,10 @@
 
 return [
   'oAuth' => [
-<<<<<<< HEAD
-      'host' => 'tcp://authserver.mddcrl.0001.usw1.cache.amazonaws.com',
-  ],    
+    'host' => 'tcp://authserver.mddcrl.0001.usw1.cache.amazonaws.com',      
+    'database' => 2,      
+  ],
     'pingAsPresence' => [
-        //'host' => 'tcp://ping-as-presence.mddcrl.0001.usw1.cache.amazonaws.com',
-        'host' => 'tcp://ping-as-presence.mddcrl.0001.usw1.cache.amazonaws.com',
-    ],
+      'host' => 'tcp://ping-as-presence.mddcrl.0001.usw1.cache.amazonaws.com',
+  ],	 
 ];
-=======
-      'host' => 'tcp://authserver.mddcrl.0001.usw1.cache.amazonaws.com',      
-      'database' => 2,      
-  ]  
-];
->>>>>>> upstream/master
