66078c263e02f3163b0d52c1f8cebb7016278f0c
ivgam <ivan.garcia.maya@gmail.com>
2015-01-09 11:32:05 -0800

    handleError

--- app/Lib/AppError.php
+++ app/Lib/AppError.php
@@ -5,21 +5,31 @@ class AppError
 
     public static function handleError ($code, $description, $file = null, $line = null, $context = null)
     {
-        if (extension_loaded('newrelic')) {
-            newrelic_notice_error($code);
-        }
+        NewRelicComponent::noticeError($error, 
+            [
+                'error_no'          => $code,
+                'error_description' => $description,
+                'error_file'        => $file,
+                'error_line'        => $line,
+                'error_context'     => $context,
+            ]
+        );
         return ErrorHandler::handleError($code, $description, $file, $line, $context);
     }
 
     public static function handleException (Exception $error)
-    {              
+    {
         switch (get_class($error)) {
-            case 'Swarm\BadRequestException'          : $code = 400; break;
-            case 'Swarm\NotFoundException'            : $code = 404; break;
-            case 'Swarm\UnauthorizedException'        : $code = 401; break;
-            case 'Swarm\UnprocessableEntityException' : $code = 422; break;
-            case 'Swarm\ApplicationErrorException'    :
-            default                                   : $code = (!empty($error->getCode())) ? $error->getCode() : 500;
+            case 'Swarm\BadRequestException' : $code = 400;
+                break;
+            case 'Swarm\NotFoundException' : $code = 404;
+                break;
+            case 'Swarm\UnauthorizedException' : $code = 401;
+                break;
+            case 'Swarm\UnprocessableEntityException' : $code = 422;
+                break;
+            case 'Swarm\ApplicationErrorException' :
+            default : $code = (!empty($error->getCode())) ? $error->getCode() : 500;
         }
         $description = ($code == 404) ? 'Not Found' : $error->getMessage();
         $error_no    = (!empty($error->getCode())) ? $error->getCode() : 999;
@@ -32,22 +42,21 @@ class AppError
         header("Pragma: no-cache");
         header("Cache-Control: no-store; no-cache;must-revalidate; post-check=0; pre-check=0");
         echo json_encode(
-            [
-                'error'             => $error_no,
-                'error_description' => $description
-            ]
+                [
+                    'error'             => $error_no,
+                    'error_description' => $description
+                ]
         );
-        NewRelicComponent::noticeError($error,  
-            [
-                'error_no'          => $error_no,
-                'error_description' => $description,
-                'request_uri'       => $_SERVER['REQUEST_URI'],
-                'request_params'    => json_encode($_POST),
-                'exception_type'    => get_class($error),
-                'exception_file'    => $error->getFile(),
-                'exception_line'    => $error->getLine(),                
-            ]
-        );     
+        NewRelicComponent::noticeError($error, [
+            'error_no'          => $error_no,
+            'error_description' => $description,
+            'request_uri'       => $_SERVER['REQUEST_URI'],
+            'request_params'    => json_encode($_POST),
+            'exception_type'    => get_class($error),
+            'exception_file'    => $error->getFile(),
+            'exception_line'    => $error->getLine(),
+                ]
+        );
         die();
     }
 
